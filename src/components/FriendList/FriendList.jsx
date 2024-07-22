e0a5f5b4da835155259594efbe4b82a99a1f7e4d
prihodko_bohdan <prihodkob15@gmail.com>
2024-07-22 17:13:41 +0300

    fixed FriendList key and Li

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,21 +1,20 @@
 import FriendListItem from "../FriendListItem/FriendListItem"
 import css from "./FriendList.module.css"
 
-const FriendList = ({ friends, }) => {
-    return (
-      <ul className={css.friendList}>
-        <li className={css.friendItem}>
-          {friends.map((friend) => (
-            <FriendListItem
-              avatar={friend.avatar}
-              name={friend.name}
-              isOnline={friend.isOnline}
-              key={friend.id}
-            />
-          ))}
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map((friend) => (
+        <li className={css.friendItem} key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
         </li>
-      </ul>
-    );
+      ))}
+    </ul>
+  );
 };
 
-export default FriendList
+export default FriendList;
